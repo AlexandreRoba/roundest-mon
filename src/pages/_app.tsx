759145b4a26cb1d9a599d41514649637150451de
Alexandre Roba <alexandre.roba@arobanet.be>
2022-12-05 21:49:34 +0100

    Add trpc

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,12 @@
-import type { AppProps } from 'next/app'
-import 'tailwindcss/tailwind.css'
 import '../styles/global.css'
+import 'tailwindcss/tailwind.css'
+import type {AppProps, AppType} from 'next/app'
+import {trpc} from '@/utils/trpc';
 
-export default function App({ Component, pageProps }: AppProps) {
+const MyApp: AppType = ({Component, pageProps}: AppProps) => {
   return <Component {...pageProps} />
 }
+
+export default trpc.withTRPC(MyApp);
+
+
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,20 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import {trpc} from "@/utils/trpc";
 
 export default function Home() {
+  const {data, isLoading} = trpc.hello.useQuery({text: "Theo"})
+  if (isLoading) return <div>Loading...</div>
+  if (data) return <div>{data.greeting}</div>
   return (
-    <div className="h-screen w-screen flex flex-col justify-center items-center">
-      <div className="text-2xl text-center">Which pokemon is Rounder?</div>
-      <div className="p-2"/>
-      <div className="border rounded p-8 flex justify-between max-w-2xl items-center">
-        <div className="w-16 h-16 bg-red-200"></div>
-        <div className="p-8">VS</div>
-        <div  className="w-16 h-16 bg-red-200"></div>
+      <div className="h-screen w-screen flex flex-col justify-center items-center">
+        <div className="text-2xl text-center">Which pokemon is Rounder?</div>
+        <div className="p-2"/>
+        <div className="border rounded p-8 flex justify-between max-w-2xl items-center">
+          <div className="w-16 h-16 bg-red-200"></div>
+          <div className="p-8">VS</div>
+          <div className="w-16 h-16 bg-red-200"></div>
+        </div>
       </div>
-    </div>
   )
 }
