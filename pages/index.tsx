c15eadff5bca88ae8cdd1fcea90d24a71e313109
Alexandre Roba <alexandre.roba@arobanet.be>
2022-12-05 21:08:48 +0100

    Basic UI structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,14 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className='text-2xl text-red-200'>Hello world</div>
+    <div className="h-screen w-screen flex flex-col justify-center items-center">
+      <div className="text-2xl text-center">Which pokemon is Rounder?</div>
+      <div className="p-2"/>
+      <div className="border rounded p-8 flex justify-between max-w-2xl items-center">
+        <div className="w-16 h-16 bg-red-200"></div>
+        <div className="p-8">VS</div>
+        <div  className="w-16 h-16 bg-red-200"></div>
+      </div>
+    </div>
   )
 }
